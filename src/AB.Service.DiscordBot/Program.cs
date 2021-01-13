28de9728e301586720845ab404b6ee720791c08d
Robert Kirkham <rsk1588.10+github@gmail.com>
2021-01-13 18:00:36 +0000

    Add DiscordBot service.

---
+++ src/AB.Service.DiscordBot/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AB.Service.DiscordBot
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World {args}!");
+        }
+
+        #endregion Public Methods
+    }
+}
